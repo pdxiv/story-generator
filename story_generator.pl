e4c6e55a5062b54fa844909a1ff48bed79efc236
pdxiv <pdxiv@github.com>
2020-09-20 14:56:59 +0200

    change to new flexible tagging format

--- story_generator.pl
+++ story_generator.pl
@@ -4,65 +4,36 @@ use warnings;
 
 my @story_template = <STDIN>;
 
-my $location_pattern = qr/(\{([^\}]+)\})/;
-my @location;
-my $thing_pattern = qr/(\[([^\]]+)\])/;
-my @thing;
-my $person_pattern = qr/(\(([^\)]+)\))/;
-my @person;
-my $action_pattern = qr/(\<([^\>]+)\>)/;
-my @action;
+# We're using a dynamic tagging pattern. Something like: <person:Bob>.
+my $tagging_pattern = qr/<(\w+):([^>]*)>/;
 
-# Make a list of locations, things and persons
-my ( %location, %thing, %person, %action );
+# Generate statistics over tags
+my %tag_stats;
 foreach my $template_line (@story_template) {
-    while ( $template_line =~ /$location_pattern/g ) {
-        $location{$2}++;
+    while ( $template_line =~ /$tagging_pattern/g ) {
+        $tag_stats{$1}{$2}++;
     }
-    while ( $template_line =~ /$thing_pattern/g ) {
-        $thing{$2}++;
-    }
-    while ( $template_line =~ /$person_pattern/g ) {
-        $person{$2}++;
-    }
-    while ( $template_line =~ /$action_pattern/g ) {
-        $action{$2}++;
-    }
-
 }
-push @location, $_ foreach keys %location;
-push @thing,    $_ foreach keys %thing;
-push @person,   $_ foreach keys %person;
-push @action,   $_ foreach keys %action;
 
-# Get random location, thing and person
-my $random_location = $location[ int( rand( scalar @location ) ) ];
-my $random_thing    = $thing[ int( rand( scalar @thing ) ) ];
-my $random_person   = $person[ int( rand( scalar @person ) ) ];
-my $random_action   = $action[ int( rand( scalar @action ) ) ];
-
-# print $story_template[0];    # debug
-# print $story_template[1];    # debug
-
-# int(rand(10))
+my %tagged_phrase_list;
+foreach my $tag ( keys %tag_stats ) {
+    foreach my $phrase ( keys %{ $tag_stats{$tag} } ) {
+        push @{ $tagged_phrase_list{$tag} }, $phrase;
+    }
+}
 
 my $story_line = $story_template[ int( rand( scalar @story_template ) ) ];
 
-while ( $story_line =~ /$location_pattern/ ) {
-    my $random_location = $location[ int( rand( scalar @location ) ) ];
-    $story_line =~ s/$location_pattern/$random_location/;
-}
-while ( $story_line =~ /$thing_pattern/ ) {
-    my $random_thing = $thing[ int( rand( scalar @thing ) ) ];
-    $story_line =~ s/$thing_pattern/$random_thing/;
-}
-while ( $story_line =~ /$person_pattern/ ) {
-    my $random_person = $person[ int( rand( scalar @person ) ) ];
-    $story_line =~ s/$person_pattern/$random_person/;
-}
-while ( $story_line =~ /$action_pattern/ ) {
-    my $random_action = $action[ int( rand( scalar @action ) ) ];
-    $story_line =~ s/$action_pattern/$random_action/;
+while ( $story_line =~ /$tagging_pattern/ ) {
+    my $random_phrase = random_phrase_by_tag($1);
+    $story_line =~ s/$tagging_pattern/$random_phrase/;
 }
 
-print $story_line;
+print "$story_line\n";
+
+sub random_phrase_by_tag {
+    my $tag         = shift;
+    my @phrase_list = @{ $tagged_phrase_list{$tag} };
+    my $phrase      = $phrase_list[ int( rand( scalar @phrase_list ) ) ];
+    return $phrase;
+}
